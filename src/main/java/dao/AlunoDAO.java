4cd87d4f1119a3553217245d7c48d8074e280874
feren <feren@10.146.194.136>
2026-04-14 21:08:39 -0300

    init: Criação do sistema.

---
+++ src/main/java/dao/AlunoDAO.java
@@ -0,0 +1,13 @@
+/*
+Aqui vamos simular a persistência de dados.
+Nas próximas aulas nós vamos reprogramar esta classe para conectar-se com o banco de dados.
+ */
+package dao;
+
+import model.Aluno;
+import java.util.*;
+
+public class AlunoDAO {
+
+    public static ArrayList<Aluno> MinhaLista = new ArrayList<Aluno>();
+}
